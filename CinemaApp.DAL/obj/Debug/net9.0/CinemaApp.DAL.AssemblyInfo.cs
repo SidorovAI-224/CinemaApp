e0c278e9de5d172b997f6a9592c70dcdec1d2bc1
CrashPine <grekovit7@gmail.com>
2025-02-10 15:27:54 +0300

    I am praying to God

--- CinemaApp.DAL/obj/Debug/net9.0/CinemaApp.DAL.AssemblyInfo.cs
+++ CinemaApp.DAL/obj/Debug/net9.0/CinemaApp.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaApp.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1b0f498d328b61b262c519c6d291e548538b10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6adb7c415dac0371c4e71a02ed677cbbf0fef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaApp.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaApp.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
