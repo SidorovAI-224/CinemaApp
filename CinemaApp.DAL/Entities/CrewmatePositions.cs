491f89a82b0088a54f49df296b4a07304660c0ea
arturio <arturiosxd@gmail.com>
2025-02-11 21:46:03 +0200

    Checkpoint for future (Working but w'out adding many actors/genres for film)

--- CinemaApp.DAL/Entities/CrewmatePositions.cs
+++ CinemaApp.DAL/Entities/CrewmatePositions.cs
@@ -7,6 +7,7 @@ namespace CinemaApp.DAL.Entities
         public int CrewmateID { get; set; }
         public int PositionID { get; set; }
 
+
         public Crewmate Crewmate { get; set; }
         public Position Position { get; set; }
     }
