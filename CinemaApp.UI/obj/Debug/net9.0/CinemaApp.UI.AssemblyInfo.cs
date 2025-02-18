7aba3d56a8c9fdd1ea35ec26f0b678fa0a3f59c5
arturio <arturiosxd@gmail.com>
2025-02-18 21:41:22 +0200

    - Final changes (no DB)

--- CinemaApp.UI/obj/Debug/net9.0/CinemaApp.UI.AssemblyInfo.cs
+++ CinemaApp.UI/obj/Debug/net9.0/CinemaApp.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaApp.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb287048a565f87aa5ded1d0a1e2e12e7e5c5fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9333bfcbb587cdacb69ca9e7326df797bebb0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaApp.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaApp.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
